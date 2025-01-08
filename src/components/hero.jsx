6f9d80a0edf909f4ab25c97f39eab8b7080a8ec6
1zzat1lla-Fayzullayev <izzatillaf495@gmail.com>
2025-01-08 12:32:43 +0500

    fixed bugs

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -2,49 +2,33 @@ import Wrapper from "../layout/wrapper";
 
 const Hero = () => {
   return (
-    <section className="relative text-white py-10 min-h-screen px-[30px] xl:px-0">
+    <section className="relative text-white py-10 h-full min-h-screen px-[30px] xl:px-0">
       <Wrapper>
         <div className="container mx-auto relative flex flex-col items-center">
-          <div className="pt-[50px] md:pt-[200px]">
-            <img
-              src="/part-earth.svg"
-              alt=""
-              className="absolute z-[-1] bottom-[-100%] md:bottom-0 left-0 right-0 mx-auto"
-            />
-            <img
-              src="/blockchan-animate-bg.svg"
-              alt=""
-              className="absolute z-[-1] md:bottom-0 left-0 right-0 mx-auto"
-            />
-            <img
-              src="/meteor-shower.webp"
-              alt=""
-              className="absolute z-[-1] md:bottom-0 left-0 right-0 mx-auto"
-              style={{ mixBlendMode: "color-dodge" }}
-            />
-          </div>
-
           <div className="w-full flex justify-center mb-8">
-            <img src="/yellow_crypta.webp" className="yellow_crypta max-w-[100%] w-[50px] md:w-auto" />
+            <img
+              src="/yellow_crypta.webp"
+              className="yellow_crypta max-w-[100%]  z-[-1] w-[50px] md:w-auto"
+            />
             <img
               src="/green_crypta.webp"
-              className="green_crypta bottom-0 left-[8%] max-w-[100%] w-[50px] md:w-auto"
+              className="green_crypta bottom-0 left-[8%] max-w-[100%] z-[-1]  w-[50px] md:w-auto"
             />
             <img
               src="/orange_crypta.webp"
-              className="orange_crypta top-0 right-[17%] max-w-[100%] w-[50px] md:w-auto"
+              className="orange_crypta top-0 right-[17%] max-w-[100%] z-[-1]  w-[50px] md:w-auto"
             />
             <img
               src="/darkgreen_right_crypta.webp"
-              className="dark_green_crypta right-0 top-[14%] max-w-[100%] w-[50px] md:w-auto"
+              className="dark_green_crypta right-0 top-[14%] max-w-[100%] z-[-1]  w-[50px] md:w-auto"
             />
             <img
               src="/blue_crypta.webp"
-              className="blue_crypta right-[8%] bottom-0 max-w-[100%] w-[50px] md:w-auto"
+              className="blue_crypta right-[8%] bottom-0 max-w-[100%] z-[-1]  w-[50px] md:w-auto"
             />
           </div>
 
-          <div className="mt-[50px]">
+          <div className="mt-[50px] flex flex-col items-center justify-start">
             <h1 className="text-4xl font-bold text-center mb-6 text-[30px] md:text-[56px] uppercase font-Neue">
               Ultimate AI Trading Ecosystem
             </h1>
@@ -60,6 +44,25 @@ const Hero = () => {
           >
             Buy Now
           </a>
+
+          <div className="pt-[50px] md:pt-[200px]">
+            <img
+              src="/part-earth.svg"
+              alt=""
+              className="absolute bottom-[-30%]  left-0 right-0 mx-auto"
+            />
+            <img
+              src="/blockchan-animate-bg.svg"
+              alt=""
+              className="absolute z-[-1] top-0 md:bottom-0 left-0 right-0 mx-auto"
+            />
+            <img
+              src="/meteor-shower.webp"
+              alt=""
+              className="absolute z-[-1] top-0 md:top-[20%] md:bottom-0 left-0 right-0 mx-auto"
+              style={{ mixBlendMode: "color-dodge" }}
+            />
+          </div>
         </div>
       </Wrapper>
     </section>
