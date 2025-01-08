d3a4f22515021d246228ac3affc70fe0de9fdc59
Forestest <forestest26@gmail.com>
2025-01-09 02:39:39 +0500

    New

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,13 +5,13 @@ const Navbar = ({handleOpenMenu}) => {
   return (
     <div className="site-header p-[1%2.5%1%2.5%] relative z-[100]">
       <div className="content flex flex-row items-center justify-between">
-        <div className="lft h-20 flex items-center p-[10px] w-1/4">
+        <div className="lft h-20 flex items-center p-[10px] lg:w-1/4">
           <img
             src="https://crypteck.jwsuperthemes.com/wp-content/uploads/2022/07/Logo.svg"
             alt="logo"
           />
         </div>
-        <div className="center-navs w-1/2 justify-center hidden md:flex">
+        <div className="center-navs w-1/2 justify-center hidden lg:flex">
           <div className="nav-links font-Apfel text-[#F3F3F3] text-[15px] flex flex-row gap-5">
             <li id="navItem" className="cursor-pointer list-none font-Apfel">
               Home
@@ -60,15 +60,15 @@ const Navbar = ({handleOpenMenu}) => {
           </div>
         </div>
 
-        <div className="end w-1/4 md:flex justify-end hidden">
+        <div className="end lg:w-1/4 lg:flex justify-end hidden">
           <button className="font-Neue text-[#151517] text-[15px] font-extrabold leading-[20px] px-[25px] py-[10px] bg-[#B7E82E]">
             Get Started
           </button>
         </div>
-        <div className="flex-col gap-1 flex md:hidden" onClick={handleOpenMenu}>
-          <div className="w-[30px] h-[2px] bg-white"></div>
-          <div className="w-[30px] h-[2px] bg-white"></div>
-          <div className="w-[30px] h-[2px] bg-white"></div>
+        <div className="flex-col gap-[3px] flex lg:hidden" onClick={handleOpenMenu}>
+          <div className="w-[30px] h-[2px] rounded-full bg-white"></div>
+          <div className="w-[30px] h-[2px] rounded-full bg-white"></div>
+          <div className="w-[30px] h-[2px] rounded-full bg-white"></div>
         </div>
       </div>
     </div>
